1470b375ceef341d5a9a4d1d8f9960825dc5343c
Bitterisland6 <bitterisland6@gmail.com>
2023-03-08 21:08:20 +0100

    data_processing: changed tests structure to fit the kedro style
    Signed-off-by: Bitterisland6 <bitterisland6@gmail.com>

---
+++ src/tests/pipelines/data_processing/test_nodes.py
@@ -0,0 +1,17 @@
+import torch
+
+from kedro_image_classification.pipelines.data_processing.nodes import load_dataset
+
+
+def test_dataset_loading():
+    cfg = {
+        "train_loader": {"batch_size": 4, "shuffle": True, "num_workers": 4},
+        "test_loader": {"batch_size": 4, "shuffle": False, "num_workers": 4},
+    }
+
+    tensor = torch.tensor([[1, 2, 3], [4, 5, 6]])
+    loaded_dataset = load_dataset(cfg, tensor)
+
+    assert len(loaded_dataset) == 2
+    assert type(loaded_dataset[0]) == torch.utils.data.DataLoader
+    assert type(loaded_dataset[1]) == torch.utils.data.DataLoader
---
+++ src/tests/pipelines/data_processing/test_pipeline.py
@@ -0,0 +1,14 @@
+from kedro.pipeline import Pipeline
+
+from kedro_image_classification.pipelines.data_processing.pipeline import (
+    create_pipeline,
+)
+
+
+def test_pipeline_creation():
+    pipeline = create_pipeline()
+
+    assert type(pipeline) == Pipeline
+    assert len(pipeline.nodes) > 0
+    assert len(pipeline.all_outputs()) == 2
+    assert len(pipeline.all_inputs()) == 2
