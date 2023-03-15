833403cafab1108de5ff999b63ebb6a52aa9f738
Bitterisland6 <bitterisland6@gmail.com>
2023-03-15 23:51:37 +0100

    data_download: added documentation to some methods
    Signed-off-by: Bitterisland6 <bitterisland6@gmail.com>

--- src/kedro_image_classification/pipelines/data_download/nodes.py
+++ src/kedro_image_classification/pipelines/data_download/nodes.py
@@ -4,16 +4,23 @@ from torchvision import datasets
 
 
 def torchvision_download():
+    """Method downloading the dataset from torchvision."""
     _ = datasets.CIFAR10("data/01_raw", download=True)
 
 
 def aws_download():
+    """Method downloading the dataset from AWS S3 bucket."""
     os.system("dvc pull")
     os.system("tar -xf data/01_raw/cifar-10-python.tar.gz -C data/01_raw/")
     os.system("rm data/01_raw/cifar-10-python.tar.gz")
 
 
-def cloud_download(cloud):
+def cloud_download(cloud: str):
+    """Method performing dataset download from desired cloud platform
+
+    Args:
+        cloud (str): cloud platform which to download the data from
+    """
     if cloud == "aws":
         aws_download()
 
