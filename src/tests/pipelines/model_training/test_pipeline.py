be51045532df527df8827942e8566f1b0025a751
Rafał Nowak <rafal.nowak@tooploox.com>
2023-04-13 12:22:27 +0200

    add TODO test

---
+++ src/tests/pipelines/model_training/test_pipeline.py
@@ -0,0 +1,8 @@
+from kedro_image_classification.pipelines.model_training.nodes import create_trainer
+
+
+class TestPipeline:
+    def test_node_create_trainer(self):
+        # create_trainer()
+        # TODO:
+        assert True
