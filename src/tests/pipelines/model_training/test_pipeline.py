95131d13aee3a589bbf301545eb68f18bdc12228
Bitterisland6 <bitterisland6@gmail.com>
2023-04-27 09:02:51 +0200

    tests: simple model_training pipeline tests
    Signed-off-by: Bitterisland6 <bitterisland6@gmail.com>

--- src/tests/pipelines/model_training/test_pipeline.py
+++ src/tests/pipelines/model_training/test_pipeline.py
@@ -1,8 +1,14 @@
-from kedro_image_classification.pipelines.model_training.nodes import create_trainer
+from kedro.pipeline import Pipeline
+
+from kedro_image_classification.pipelines.model_training.pipeline import create_pipeline
 
 
 class TestPipeline:
-    def test_node_create_trainer(self):
-        # create_trainer()
-        # TODO:
-        assert True
+    def test_pipeline_creation(self):
+        """Test checking data_processing pipeline creation."""
+
+        pipeline = create_pipeline()
+        assert type(pipeline) == Pipeline
+        assert len(pipeline.nodes) == 4
+        assert len(pipeline.all_outputs()) == len(pipeline.nodes)
+        assert len(pipeline.all_inputs()) == 9
